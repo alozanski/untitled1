ef8c2383454b57a895619470b657946fb9358ad2
alozanski <alozanski@vp.pl>
2022-04-19 20:38:37 +0200

    uptate comit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        String month = "Kwiecien";
+        int year = 2022;
+        double day = 19;
+        System.out.println("miesiąc: " + month);
+        System.out.println("Rok : " + year);
+        System.out.println("miesiąc: " + day);
+
+
+    }
+}
